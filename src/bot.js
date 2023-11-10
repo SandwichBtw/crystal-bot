72d7113cfb0596c196f2a0e999fb882cc146ee66
SandwichBtw <robinprotonme@proton.me>
2023-11-10 22:45:37 +0100

    First commit with nate

---
+++ src/bot.js
@@ -0,0 +1,19 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', bot => {
+    console.log(`Logged in as ${bot.user.tag}`);
+});
+
+client.login(`${process.env.TOKEN}`);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { ShardingManager } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const manager = new ShardingManager('./bot.js', { token: `${process.env.TOKEN}` });
+
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+
+manager.spawn();
