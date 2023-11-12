f85d17ad9d5f4b4f1b78aaeaba985bea8610fac5
Nate Reprogle <4064388+TerrrorByte@users.noreply.github.com>
2023-11-12 10:57:23 -0600

    Fix tsconfig

--- src/modules/commands/helloCommand.ts
+++ src/modules/commands/helloCommand.ts
@@ -1,16 +1,22 @@
-import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
-import { type Command } from "src/types/Command";
-import type CrystalClient from "src/types/CrystalClient";
+import {
+    SlashCommandBuilder,
+    type ChatInputCommandInteraction,
+} from "discord.js"
+import { type Command } from "../../types/Command"
+import type CrystalClient from "../../types/CrystalClient"
 
 module.exports = {
-    name: 'hello',
+    name: "hello",
     data: new SlashCommandBuilder()
-    .setName("hello")
-    .setDescription("Say hello!"),
-    execute: function(interaction: ChatInputCommandInteraction, client: CrystalClient) {
+        .setName("hello")
+        .setDescription("Say hello!"),
+    execute: function (
+        interaction: ChatInputCommandInteraction,
+        client: CrystalClient
+    ) {
         void interaction.reply({
             content: "Hey there!",
-            ephemeral: false
+            ephemeral: false,
         })
-    }
+    },
 } satisfies Command
