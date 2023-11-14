c1933b25e986d5f3828b290bab5519a2f6ec31cb
SandwichBtw <robinprotonme@proton.me>
2023-11-14 17:49:14 +0100

    Fixed pin command

--- src/modules/commands/pinCommand.ts
+++ src/modules/commands/pinCommand.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, type ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder, type ChatInputCommandInteraction, type TextChannel } from "discord.js";
 import { type Command } from "../../types/Command";
 import type CrystalClient from "../../types/CrystalClient";
 
@@ -6,8 +6,52 @@ module.exports = {
     name: 'pin',
     data: new SlashCommandBuilder()
         .setName("pin")
-        .setDescription("pin a message using the bot."),
+        .setDescription("pin a message using the bot.")
+        .addChannelOption((option) =>
+            option
+                .setName('channel')
+                .setDescription('The channel you want to pin a message in.')
+                .setRequired(true))
+        .addStringOption((option) =>
+            option
+                .setName('message_id')
+                .setDescription('The message you want to pin to using a message id.')
+                .setRequired(true)),
     execute: async function (interaction: ChatInputCommandInteraction, client: CrystalClient) {
-        
+        const textChannel = interaction.options.getChannel('channel') as TextChannel
+        const messageId = interaction.options.getString('message_id')
+
+        try {
+            if (messageId !== null) {
+                const targetMessage = await textChannel.messages.fetch(messageId)
+
+                void await targetMessage.pin()
+                void await interaction.reply({
+                    content: "Successfully pinned the message.",
+                    ephemeral: true
+                })
+            } else {
+                void await interaction.reply({
+                    content: "Unsuccessfully pinned the message.",
+                    ephemeral: true
+                })
+            }
+        } catch (error: any) {
+            switch (true) {
+                case error.message.includes('Unknown Message'):
+                    void await interaction.reply({
+                        content: "Unsuccessfully pinned the message: Invalid message id.",
+                        ephemeral: true
+                    })
+                    break
+                default:
+                    void await interaction.reply({
+                        content: "Unsuccessfully pinned the message.",
+                        ephemeral: true
+                    })
+
+                    console.error(error)
+            }
+        }
     }
 } satisfies Command
