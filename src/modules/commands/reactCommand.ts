d067f5eae9030e2e9406230ee928cd4d73b1309f
SandwichBtw <robinprotonme@proton.me>
2023-11-14 17:26:44 +0100

    Added error messages and support for multiple emojis in the react command

--- src/modules/commands/reactCommand.ts
+++ src/modules/commands/reactCommand.ts
@@ -19,19 +19,23 @@ module.exports = {
                 .setRequired(true))
         .addStringOption((option) =>
             option
-                .setName('emoji')
-                .setDescription('The emoji you want to react with.')
+                .setName('emojis')
+                .setDescription('The emoji(s) you want to react with.')
                 .setRequired(true)),
     execute: async function (interaction: ChatInputCommandInteraction, client: CrystalClient) {
         const textChannel = interaction.options.getChannel('channel') as TextChannel
         const messageId = interaction.options.getString('message_id')
-        const reactEmoji = interaction.options.getString('emoji')
+        const emojis = interaction.options.getString('emojis')
 
         try {
-            if ((reactEmoji !== null) && (messageId !== null)) {
-                const targetMessage = await textChannel.messages.fetch(messageId);
+            if ((emojis !== null) && (messageId !== null)) {
+                const targetMessage = await textChannel.messages.fetch(messageId)
+                const emojiArray = emojis.split(' ')
 
-                void await targetMessage.react(reactEmoji)
+                for (const emoji of emojiArray) {
+                    void await targetMessage.react(emoji.trim())
+                }
+
                 void await interaction.reply({
                     content: "The reaction was successful.",
                     ephemeral: true
@@ -42,8 +46,28 @@ module.exports = {
                     ephemeral: true
                 })
             }
-        } catch (error) {
-            console.error(error)
+        } catch (error: any) {
+            switch (true) {
+                case error.message.includes('Unknown Message'):
+                    void await interaction.reply({
+                        content: "The reaction failed: Invalid message id.",
+                        ephemeral: true
+                    })
+                    break
+                case error.message.includes('Unknown Emoji'):
+                    void await interaction.reply({
+                        content: "The reaction failed: Invalid emoji(s).",
+                        ephemeral: true
+                    })
+                    break
+                default:
+                    void await interaction.reply({
+                        content: "The reaction failed.",
+                        ephemeral: true
+                    })
+
+                    console.error(error)
+            }
         }
     }
 } satisfies Command
