9b54033a1ea321009e3c26cdd07f2399d5a5fda6
Nate Reprogle <4064388+TerrrorByte@users.noreply.github.com>
2023-11-14 21:40:07 -0600

    Clearly it does because it broke now lol. Reverting change

--- src/modules/events/ready.ts
+++ src/modules/events/ready.ts
@@ -1,15 +1,15 @@
 import { type Event } from "../../types/Event"
 import { loadCommands } from "../utils/commandHandler"
 import { getChannels } from "../utils/channelRegistrar"
-import CrystalClient from "../../types/CrystalClient"
+import type CrystalClient from "../../types/CrystalClient"
 
 module.exports = {
     name: "ready",
     once: false,
     rest: false,
     execute: async function (client: CrystalClient): Promise<void> {
-        await loadCommands(CrystalClient.getClient())
-        await getChannels(CrystalClient.getClient())
+        await loadCommands(client)
+        await getChannels(client)
 
         console.log(`Logged in as ${client.user?.tag}`)
     },
