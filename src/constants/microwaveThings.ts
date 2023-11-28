c0b22eb9b453ff93b4bfce13a3a3f18a8f024200
SandwichBtw <robinprotonme@proton.me>
2023-11-28 18:38:11 +0100

    Updated messageScheduler

---
+++ src/constants/microwaveThings.ts
@@ -0,0 +1,7 @@
+export enum microwaveThings {
+    n1 = "Phone",
+    n2 = "Car",
+    n3 = "Roblox account",
+    n4 = "Shoes",
+    n5 = "Nintendo switch"
+}
--- src/modules/utils/messageScheduler.ts
+++ src/modules/utils/messageScheduler.ts
@@ -2,16 +2,36 @@ import schedule from "node-schedule"
 import type CrystalClient from "../../types/CrystalClient"
 import { type TextChannel } from "discord.js"
 import { getConfig } from "../config"
+import { microwaveThings } from "../../constants/microwaveThings"
 
 const config = getConfig()
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function loadSchedules(client: CrystalClient): Promise<void> {
-    schedule.scheduleJob('*/1 * * * *', testMessage)
+    // schedule.scheduleJob('26 8 * * 5', microwave)
+    schedule.scheduleJob('30 18 * * 2', microwave)
+    schedule.scheduleJob('34 13 * * 1,4,6', desksAndChairs)
+    schedule.scheduleJob('12 5 * * 1,4,6', desksAndChairs)
+    schedule.scheduleJob('56 16 * * 1,4,6', desksAndChairs)
+    schedule.scheduleJob('0 12 */8 * *', polls)
 
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    async function testMessage() {
+    async function microwave() {
         const channel = await client.channels.fetch(config.channelId) as TextChannel
-        await channel.send("This runs every 1 minute.")
+        const values = Object.values(microwaveThings);
+        const randomIndex = Math.floor(Math.random() * values.length);
+        const randomItem = values[randomIndex];
+
+        await channel.send(`Gosh darn it! I forgot my ${randomItem} in the microwave again!`)
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    async function desksAndChairs() {
+
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    async function polls() {
+
     }
 }
